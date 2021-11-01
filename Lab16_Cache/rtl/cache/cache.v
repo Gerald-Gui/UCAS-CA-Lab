84ab5217ac36e863d65ef9495227aabc6dcac6e7
Gerald-Gui <guitinghui19@mails.ucas.ac.cn>
2021-11-01 14:57:34 +0800

    lab16: init cache module ports

--- Lab16_Cache/rtl/cache/cache.v
+++ Lab16_Cache/rtl/cache/cache.v
@@ -1,3 +1,34 @@
-module cache();
+module cache(
+        input  clk_g,
+        input  resetn,
+
+        input           valid,
+        input           op,
+        input  [ 7:0]   index,
+        input  [19:0]   tag,
+        input  [ 3:0]   offset,
+        input  [ 3:0]   wstrb,
+        input  [31:0]   wdata,
+
+        output          addr_ok,
+        output          data_ok,
+        output [31:0]   rdata,
+
+
+        output          rd_req,
+        output [ 2:0]   rd_type,
+        output [31:0]   rd_addr,
+        input           rd_rdy,
+        input           ret_valid,
+        input  [ 2:0]   ret_last,
+        input  [31:0]   ret_data,
+
+        output          wr_req,
+        output [  2:0]  wr_type,
+        output [ 31:0]  wr_addr,
+        output [  3:0]  wr_wstrb,
+        output [127:0]  wr_data,
+        input           wr_rdy
+    );
 
 endmodule
